7c203b369e4860e788e014fcd93ae6f36a7dfa9e
dario <dariogal@ucm.es>
2019-05-30 16:42:11 -0700

    View login finished, now i need to completed the next view, that is register. Also we must to change the image to login with a Jumbi image. Delete to navigation bar to login and add to modal show connection.

---
+++ Jumbi/DesignedView.swift
@@ -0,0 +1,48 @@
+//
+//  DesignedView.swift
+//  Jumbi
+//
+//  Created by dario on 30/05/2019.
+//  Copyright © 2019 dario. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableView: UIView {
+    
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+}
