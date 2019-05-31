ab9d70e1066d01c964353133dd5a6f151b4059f2
dario <dariogal@ucm.es>
2019-05-31 16:24:10 -0700

    Search View table filter to product for name or category , but the name is necessary to be complete and you don't find a product with the compose name, if  the selection is the first name.

--- Jumbi/SearchTableViewController.swift
+++ Jumbi/SearchTableViewController.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class SearchTableViewController: UIViewController {
 
+    //    var countryList = ["Aceite usado de vehículos","Aceite usado de cocina","Aparatos electrónicos ","Auriculares","Baterías","Cascos","Disolventes","Escombros","Fluorescentes","Instrumentos de oficina", "Medicamentos","Mecheros", "Pilas", "Pinturas","Radiografías"]
     
+
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tblView: UITableView!
     
     var trushies = [Trush]()
+    var filteredTrush = [Trush]()
     let searchController = UISearchController(searchResultsController: nil)
-    
-//    var countryList = ["Aceite usado de vehículos","Aceite usado de cocina","Aparatos electrónicos ","Auriculares","Baterías","Cascos","Disolventes","Escombros","Fluorescentes","Instrumentos de oficina", "Medicamentos","Mecheros", "Pilas", "Pinturas","Radiografías"]
-    
-    
-    var searchedCountry = [Trush]()
     var searching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // Setup the Search Controller
+        //searchController.searchResultsUpdater = self
+        //searchController.obscuresBackgroundDuringPresentation = false
+        //searchController.searchBar.placeholder = "Search trushies"
+        //navigationItem.searchController = searchController
+        definesPresentationContext = true
         
         trushies = [
             Trush(category:"Chocolate", name:"Chocolate Bar"),
@@ -37,93 +43,96 @@ class SearchTableViewController: UIViewController {
             Trush(category:"Other", name:"Sour Chew"),
             Trush(category:"Other", name:"Gummi Bear"),
             Trush(category:"Other", name:"Candy Floss"),
-            Trush(category:"Chocolate", name:"Chocolate Coin"),
+            Trush(category:"Chocolate", name:"Coin"),
             Trush(category:"Chocolate", name:"Chocolate Egg"),
             Trush(category:"Other", name:"Jelly Beans"),
             Trush(category:"Other", name:"Liquorice"),
             Trush(category:"Hard", name:"Toffee Apple")]
+        
+
     }
     
 }
 
 extension SearchTableViewController: UITableViewDelegate, UITableViewDataSource {
     
-    func searchBarIsEmpty() -> Bool {
-        return searchController.searchBar.text?.isEmpty ?? true
-    }
-    func isFiltering() -> Bool {
-        let searchBarScopeIsFiltering = searchController.searchBar.selectedScopeButtonIndex != 0
-        return searchController.isActive && (!searchBarIsEmpty() || searchBarScopeIsFiltering)
-    }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if searching {
-           return searchedCountry.count
+           return filteredTrush.count
         } else {
            return trushies.count
        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-//        if searching {
-//            cell?.textLabel?.text = searchedCountry[indexPath.row]
-//        } else {
-//            cell?.textLabel?.text = countryList[indexPath.row]
-//        }
-//        return cell!
-        
-        
-        
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
         let trush: Trush
         if searching {
-            trush = searchedCountry[indexPath.row]
+            trush = filteredTrush[indexPath.row]
         } else {
             trush = trushies[indexPath.row]
         }
         cell.textLabel!.text = trush.name
         cell.detailTextLabel!.text = trush.category
+        
         return cell
     }
     
+    
+    func searchBarIsEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func isFiltering() -> Bool {
+        let searchBarScopeIsFiltering = searchController.searchBar.selectedScopeButtonIndex != 0
+        return searchController.isActive && (!searchBarIsEmpty() || searchBarScopeIsFiltering)
+    }
+    
+    
+    
 }
-
+// MARK : filtrado por categoria y nombre.
 
 extension SearchTableViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         // cuando pulsamos sobre la barra se abre el teclado
-        self.view.endEditing(true)
+        //self.view.endEditing(true)
         
-//        searchedCountry = countryList.filter(
-//            {$0.lowercased().prefix(searchText.count) == searchText.lowercased()}
-//        )
-        
-        
-        searchedCountry = trushies.filter({( trush : Trush) -> Bool in
-            let doesCategoryMatch = (scope == "All") || (trush.category == scope)
-            
-            if searchBarIsEmpty() {
-                return doesCategoryMatch
-            } else {
-                return doesCategoryMatch && trush.name.lowercased().contains(searchText.lowercased())
+        filteredTrush = trushies.filter(
+            {   $0.category.lowercased().prefix(searchText.count) == searchText.lowercased() ||
+                $0.name.lowercased().prefix(searchText.count) == searchText.lowercased()
             }
-        })
+        )
         
+        debugPrint(filteredTrush)
         searching = true
         tblView.reloadData()
     }
+    
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searching = false
         searchBar.text = ""
         tblView.reloadData()
         //cuando le damos a cancelar desaparece el teclado
         self.view.endEditing(false)
+    }
+}
+// MARK :  hay que hacer una factoria similar a Plate para buscar los trushies
+
+extension SearchTableViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        if let searchText =  searchController.searchBar.text {
+            debugPrint("Han escrito \(searchText)")
+            filteredTrush = [Trush(category: "\(searchText)",name: "producto buscado")]
+        }
+        tblView.reloadData()
     }
 }
 
