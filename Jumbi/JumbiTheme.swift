2940fd5ebbccf8b1a189edf37d2235a3bb4fc0dd
dario <dariogal@ucm.es>
2019-06-02 15:54:48 -0700

    Finished the launch screen view and  connected the all views. Now i try to the product image ,when you tap it, it go the a new view controller.

--- Jumbi/JumbiTheme.swift
+++ Jumbi/JumbiTheme.swift
@@ -9,5 +9,5 @@
 import UIKit
 
 extension UIColor {
-    static let jumbiOrange = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+    static let jumbiOrange = #colorLiteral(red: 0.9998938441, green: 0.567543149, blue: 0.302090764, alpha: 1)
 }
